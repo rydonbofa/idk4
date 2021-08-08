14201739542fdbd2f6fabab4ce6809a9b03a4617
Bastian Lobe <Bastian.lobe@t-online.de>
2021-08-08 11:48:55 +0200

    Created Server List

--- hideme/hideme.py
+++ hideme/hideme.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import bs4
+import json
 
 
 def get_running_services():
@@ -9,31 +10,3 @@ def get_running_services():
     return list(filter(None,subprocess.check_output(command, shell=True).decode("UTF-8").split("\n")))
 
 
-
-with open("temp.html", "r") as f:
-    temp = f.read()
-
-
-soup = bs4.BeautifulSoup(temp, features="html.parser")
-
-
-data = []
-table = soup.find('table')
-table_body = table.find('tbody')
-
-rows = table_body.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    for col in cols:
-        if "Linux" in col or "Details" in col or col == "":
-            cols.remove(col)
-    img = row.find('img')
-    if img:
-        cols.append(img['src'])
-    if len(cols) > 1:
-        data.append([ele for ele in cols if ele]) # Get rid of empty values
-
-
-print(data)
-#print(result)
